b36803359c2070de5b686d0f2a049e4b663aaa78
mseptiann01 <92156458+mseptiann01@users.noreply.github.com>
2022-09-21 10:58:06 +0700

    Update view.php

--- view.php
+++ view.php
@@ -1,7 +1,7 @@
 <?php
 
 $bulan = array("Januari", "Febuari", "Maret", "April", "Mei");
-$angka = [1,2,3,4,5,6,7,8,9,10,11];
+$angka = [1,2,3,4,5];
 
 echo "ini isi array bulan   :   </br>";
 var_dump($bulan);
